d9f132e5d1dc127bf1bd926181263704f3482c18
kulkarniatharv <atharvpkulkarni1108@gmail.com>
2020-11-22 00:33:00 +0530

    dynamic port number for heroku

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.use('/api', (req, res) => {
   })(req, res);
 });
 
-app.listen('3000', () => {
+app.listen(process.env.PORT || '3000', () => {
   console.log('server started on port 3000');
 });
